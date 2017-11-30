67598cf4c85829388f127b526cadfafe0ed2b3c6
Oanacozma <cozmaoanna@yahoo.com>
2017-11-30 14:55:50 +0000

    code working

--- medicalPractice/urls.py
+++ medicalPractice/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url, include
 from .views import all_doctors, do_search, practice_details
 from booking.views import booking
+# from checkout.views import checkout
 
 urlpatterns = [
     url(r'^$', all_doctors, name='medicalPractice'),
     url(r'^search/', do_search, name='search'),
     url(r'^(\d+)$', practice_details, name='practice_details'),
     url(r'^(\d+)/booking$', booking, name='booking'),
+    
+    
+    
+    # url(r'^(\d+)/checkout$', checkout, name='checkout'),
+
 ]
