9e55a584da6e877b76372e03defcf54a61e19864
Oanacozma <cozmaoanna@yahoo.com>
2017-12-12 22:07:33 +0000

    heroku

--- connectDOC/settings.py
+++ connectDOC/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'connectDOC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite")) }
-# DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
+# DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite")) }
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL")) }
 
 
 # DATABASES = {
